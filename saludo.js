a841dd39367d430cffd41e13b1fee65ab0042982
bladimirw <114620648+bladimirw@users.noreply.github.com>
2024-09-30 19:43:09 -0600

    Enviando las modificaciones en la exportacion

--- saludo.js
+++ saludo.js
@@ -2,7 +2,13 @@ function saludar(name) {
   return `Hola ${name}!`;
 }
 
+function despedir(name) {
+  return `Adiós ${name}!`;
+}
 
 
-module.exports.saludar = saludar;
+module.exports = {  
+  saludar: saludar,
+  despedir: despedir
+};
 
