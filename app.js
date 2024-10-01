dcaa35107157556cc034b80357da2eb00c532097
bladimirw <114620648+bladimirw@users.noreply.github.com>
2024-09-30 20:51:13 -0600

    Cambios desconocidos

--- app.js
+++ app.js
@@ -7,12 +7,9 @@ console.log(hola.Saludo('Angel'));
 
 console.log(hola.Despedida('Miguel'));
 
-<<<<<<< HEAD
 console.log(hola.Curso());
 
-=======
 console.log(hola.HolaMundo());
->>>>>>> 0c4d99e5981363eb6d450f332648dbb8ee594696
 
 console.log(hola.alerta());
 
